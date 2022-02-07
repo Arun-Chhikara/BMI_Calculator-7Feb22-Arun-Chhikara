ea769ff1d90535c3ccaf8b0355beab1c983dff8c
Arun-Chhikara <64885325+Arun-Chhikara@users.noreply.github.com>
2022-02-07 13:23:59 +0530

    Update bmi_cl.py

--- bmi/bmi_cl.py
+++ bmi/bmi_cl.py
@@ -3,8 +3,8 @@ import pandas as pd
 class BMI:
     '''
     To find out the BMI of a person given its Gender, Hight and Weight:-
-    a>. Use profile_bmi method to calculate the BMI of persons and also count of overweight persions
-    b>. profile_bmi ouputs a tuble of a pandas dataframe and in integer. 
+    a>. Use profile_bmi method to calculate the BMI of persons and also count of overweight persons
+    b>. profile_bmi ouputs a tuble of a pandas dataframe and an integer. 
         i). Dataframe includes 3 new columns. One for BMI, second for risk and third for weight category
         ii). Integer is the number of overwight persion in the provided data
     c>. Use input_data attribute of instanciated BMI class to acccess the original data provided as input
